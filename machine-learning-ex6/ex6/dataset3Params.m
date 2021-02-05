38a03c1d154cc709514f5ed06e265c8602929cf4
jo1jun <andrew6072@naver.com>
2021-02-05 14:35:22 +0900

    renew

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -24,58 +24,32 @@ sigma = 0.3;
 %
 
 % landmark 선택은 x data 그자체로 시작
-##x1 = X;
-##x2 = X;
-##
-##values = [0.01 0.03 0.1 0.3 1 3 10 30];
-##means = zeros(64, 1);
-##
-##k = 1;
-##for i=1:8,
-##  for j=1:8,
-##    C = values(i);
-##    sigma = values(j);
-##    model= svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma)); 
-##    predictions = svmPredict(model, Xval);
-##    means(k,1) = mean(double(predictions ~= yval));
-##    k++;
-##  endfor
-##endfor
-##
-##[val, idx] = min(means);
-##
-##if idx <= 8,
-##  i = 1;
-##  j = idx;
-##elseif idx <= 16,
-##  i = 2;
-##  j = idx - 8;
-##  elseif idx <= 24,
-##  i = 3;
-##  j = idx - 16;
-##  elseif idx <= 32,
-##  i = 4;
-##  j = idx - 24;
-##  elseif idx <= 40,
-##  i = 5;
-##  j = idx - 32;
-##   elseif idx <= 48,
-##  i = 6;
-##  j = idx - 40;
-##   elseif idx <= 56,
-##  i = 7;
-##  j = idx - 48;
-##   elseif idx <= 64,
-##  i = 8;
-##  j = idx - 56;
-##endif
-##
-##
-##C = values(i);
-##sigma = values(j);
+x1 = X;
+x2 = X;
 
-C = 1;
-sigma = 0.1;
+values = [0.01 0.03 0.1 0.3 1 3 10 30];
+means = zeros(64, 1);
+idxs = zeros(64, 2);
+
+k = 1;
+for i=1:8,
+  for j=1:8,
+    C = values(i);
+    sigma = values(j);
+    model= svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma)); 
+    predictions = svmPredict(model, Xval);
+    means(k,1) = mean(double(predictions ~= yval));
+    idxs(k,1) = i;
+    idxs(k,2) = j;
+    k++;
+  endfor
+endfor
+
+[val, idx] = min(means);
+
+C = values(idxs(idx,1));
+sigma = values(idxs(idx,2));
+
 % =========================================================================
 
 end
